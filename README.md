63b5f02de8ccfd9395eb72ffa6e36fe472770371
Dragan Markovic <dragan224@gmail.com>
2021-02-05 16:28:08 +0100

    this might be useful in the future

---
+++ README.md
@@ -0,0 +1,9 @@
+#Reddit WallStreetBets Stock Analyzer
+
+Script that spits out most commonly talked about stock symbols in Reddit's WallStreetBets forum.
+Stock symbols are read from a text file - symbols.txt
+
+Usage:
+```
+python3 stock_analyzer.py <number_of_top_posts_to_scan>
+```
---
+++ stock_analyzer.py
@@ -0,0 +1,53 @@
+
+import praw
+import sys
+
+class stock_analyzer(object):
+
+    def __init__(self):
+        self.user_agent = ("Stock analyzer")
+        self.r = praw.Reddit(user_agent=self.user_agent, client_id='gLYszkvAMDEmCg', client_secret='lHNozyOHkP7eMUOlEJiZnw6rnP1DxA')
+        self.last_day_of_current_month = 0
+        self.month_of_last_submission = 0
+        self.submission_for_month = False
+        self.main()
+        
+    def read_symbols(self):
+        f = open("./symbols.txt", "r")
+        self.symbol_dict = {x.strip().upper(): 0 for x in f.readlines()}
+
+    def get_subreddit_subs(self, subreddit):
+        wsb = self.r.subreddit(subreddit)
+        self.submissions = wsb.hot(limit=self.limit)
+        
+    def populate_symbols_dict(self):
+        for submission in self.submissions:
+            title = submission.title.upper()
+
+            for word in title.split():
+                if word in self.symbol_dict.keys():
+                    self.symbol_dict[word] += 1
+
+    def sort_symbols_mentions(self):
+        self.symbol_dict = dict(sorted(self.symbol_dict.items(), key=lambda item: item[1], reverse=True))
+
+    def print_stock_stats(self, symbol): 
+        print ('%s Mentions: %d' %(symbol.title().upper(), self.symbol_dict[symbol]))
+
+    def build_output(self):
+        print ('Scanned top %d submissions from r/wallstreetbets' % self.limit)
+        for key in self.symbol_dict.keys():
+            if self.symbol_dict[key] > 0:
+                self.print_stock_stats(key)
+
+    def main(self):
+        self.limit = int(sys.argv[1])
+
+        self.read_symbols()
+        self.get_subreddit_subs("wallstreetbets")
+        self.populate_symbols_dict()
+        self.sort_symbols_mentions()
+        self.build_output()
+
+if __name__ == "__main__":
+    stock_analyzer()
